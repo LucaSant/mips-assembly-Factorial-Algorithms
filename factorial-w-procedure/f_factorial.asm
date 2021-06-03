3040fa5e71bed3ec8753f5cc2d838ab50e102ccb
Lucas H Sant Anna <lucas.hsantanna@gmail.com>
2021-06-03 17:35:55 -0300

    - errors fixed : forgotten syscall and end programm inserted - changing the loop direction to descending: this gives meaning to save  register on stack -commented code

--- factorial-w-procedure/f_factorial.asm
+++ factorial-w-procedure/f_factorial.asm
@@ -1,27 +1,29 @@
 
     .text
 
-    .globl factorial
-    
+    .globl factorial 
+
+#arguments:
+# $a1 ---> input number 
 factorial:
 
-    addi $sp, $sp, -4
-    sw $a2, 0 ($sp)
-
+    addi $sp, $sp, -4 # update the sp register, reserving 1 word on the stack
+    sw $a1, 0 ($sp) #save the $a1 register's value on the reserved space of the stack 
+    # now the value of $a1 can be used as a  iterator for the loop
     li $v0, 1   # the $v0 will be the result of the process
-    li $t0, 0   #so lets put a number to increase from 0 to entered value 
+    li $t0, 1   #t0 is value for comparasion, loop stops when $a1's value be equals to $t0's value
 
 loop:
-
-    beq $a2, $t0, endloop
-    addi $t0, $t0, 1
-    mul $v0, $v0, $t0;
-    j loop
+    #decreasing iterator loop
+    beq $a1, $t0, endloop 
+    mul $v0, $v0, $a1 # $a1 is used to multiples with $v0 
+    addi $a1, $a1, -1 #decrease the iterator until $a1 be equals to 1
+    j loop # jump to loop
 
 endloop:
     
-    lw $a2, 0 ($sp)
-    addi $sp, $sp, 4
-    jr $ra
-
+    lw $a1, 0 ($sp) #now we can take back the value saved on the stack (the input value)
+    addi $sp, $sp, 4 # and restore the memory 
+    jr $ra # when procedure was colled, the return adress was saved at $ra register - now the process jump back
+           # update de pc register (pc <--- $ra)
     
--- factorial-w-procedure/main.asm
+++ factorial-w-procedure/main.asm
@@ -1,35 +1,38 @@
 # Class example number 2
 
-    .data 
+    .data # data declaration section
     .align 0
-
+# string declarations
 string_num:     .asciiz "Enter a number: "
 print_fat:      .asciiz "The factorial of "
 print_is:       .asciiz " is "
 
-    .text
-    .globl main
+    .text # process text section
+    .globl main 
 
-main:
+main: # glabal fuction main
 
     li $v0, 4
     la $a0, string_num
     syscall
 
-    li $v0, 5
+    li $v0, 5 # enter a integer value
     syscall
 
-    move $a2, $v0;
-    jal factorial
+    move $a1, $v0  # move the value to $a1 because $v0 is used in several commands
+                   # by convention, $a's registers are used as arguments to the procedure. 
+    jal factorial # call the procedure factorial 
+                  # jal transfer the control to the procedure (update de value of pc register)
 
-    move $t1, $a0
+    move $t1, $a1 
     move $t2, $v0
 
-    li $v0, 4
+# print all strings and values to create a phrase 
+    li $v0, 4 # print string
     la $a0, print_fat
     syscall
 
-    li $v0, 5
+    li $v0, 1 # print integer
     la $a0, ($t1)
     syscall
 
@@ -37,8 +40,12 @@ main:
     la $a0, print_is
     syscall
 
-    li $v0, 5
+    li $v0, 1
     la $a0, ($t2)
+    syscall
 
-    
+    li $v0, 10 #end program
+    syscall
+
+
 
